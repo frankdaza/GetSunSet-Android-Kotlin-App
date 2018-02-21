6a4e206a747c7731dc5ce69b64fd78ba069b556b
Frank Edward Daza González <frank.daza2@gmail.com>
2018-02-20 20:25:20 -0500

    SunSet app first commit

---
+++ app/src/main/java/com/frankdaza/getsunset/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.frankdaza.getsunset
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    protected fun getSunSet(view: View) {
+        var city: String = etCityName.text.toString().trim()
+        val url: String = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + city + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+    }
+
+}
